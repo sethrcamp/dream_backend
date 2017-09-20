28cace4ad01fa1bdadfde638191f48a663531c72
Seth Campbell <sethrcamp@gmail.com>
2017-09-20 12:01:22 -0400

    server config

---
+++ util/db_configs/server.php
@@ -0,0 +1,7 @@
+<?php
+$CONFIG['server_name'] = "localhost";
+$CONFIG['server_user'] = "root";
+$CONFIG['server_pass'] = "dreamers23";
+$CONFIG['server_db'] = "dream";
+$CONFIG['server_port'] = 8889;
+$CONFIG['s3_path'] = '../../../../dream_frontend/src/assets/s3_example';
